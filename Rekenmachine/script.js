5717543bda558371446a1eed0d0dc636014d7e74
Yassin <y.chakai@icloud.com>
2021-01-27 16:42:21 +0100

    Rekenmachine

---
+++ Rekenmachine/script.js
@@ -0,0 +1,34 @@
+// objects
+
+class Rekenmachine {
+
+    constructor (outputField)
+
+
+
+
+
+}
+
+
+// we roepen hier de nummers en functies op
+const nummers = document.querySelectorAll('.nummer');
+
+const clearall = document.querySelector('.clearall');
+
+const posneg = document.querySelector ('.posneg');  
+
+const modulo = document.querySelector('.modulo');
+
+const divide = document.querySelector  ('.divide');
+
+const multiply = document.querySelector('.multiply');
+
+const minus = document.querySelector('.minus');
+
+const addup = document.querySelector('.addup');
+
+const equals = document.querySelector('.equals');
+
+const period = document.querySelector('.period');
+
