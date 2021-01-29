4bfefc375a18843a41c41bbc7fcccfc2a09043a4
Yassin <y.chakai@icloud.com>
2021-01-29 21:33:16 +0100

    Rekenmachine

--- Rekenmachine/script.js
+++ Rekenmachine/script.js
@@ -1,34 +1,76 @@
-// objects
-
-class Rekenmachine {
-
-    constructor (outputField)
 
 
+function forclear() {
+    
+    document.getElementById("output").innerHTML = "0";
+    document.getElementById("ac").innerText = "AC";
 
+}
+
+function removeZero() {
+    
+    let value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+        value = " "
+        document.getElementById("output").innerHTML = value;
+    }
+}
+
+function negative()
+{  
+    let value = document.getElementById("output").innerHTML;
+    // return -Math.abs(value);
+    document.getElementById("output").innerHTML = -Math.abs(value);
+    if (value == -Math.abs(value))
+    {
+        document.getElementById("output").innerHTML = Math.abs(value);
+    }
+}
+
+function perc() {
+    
+    let value = document.getElementById("output").innerHTML;
+    value = value / 100;
+    
+    document.getElementById("output").innerHTML = value;
+
+}
+
+function fordisplay(myvalue) {
+    removeZero()
+    if (myvalue === '.' && document.getElementById("output").innerHTML.includes('.')) return
+    document.getElementById("output").innerHTML += myvalue;
+    document.getElementById("ac").innerText = "C";
+}
+
+function solved()
+ {  
+    
+    let equation = document.getElementById("output").innerHTML
+    let solved = eval(equation);
+    document.getElementById("output").innerHTML = solved;
 
 
 }
 
 
-// we roepen hier de nummers en functies op
-const nummers = document.querySelectorAll('.nummer');
 
-const clearall = document.querySelector('.clearall');
 
-const posneg = document.querySelector ('.posneg');  
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const valueEl = document.querySelector('.value');
 
-const modulo = document.querySelector('.modulo');
-
-const divide = document.querySelector  ('.divide');
-
-const multiply = document.querySelector('.multiply');
-
-const minus = document.querySelector('.minus');
-
-const addup = document.querySelector('.addup');
-
-const equals = document.querySelector('.equals');
-
-const period = document.querySelector('.period');
-
+const updateTime = () => {
+    const currentTime = new Date();
+  
+    let currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+  
+    if (currentHour > 24) {
+      currentHour -= 24;
+    }
+    hourEl.textContent = currentHour.toString();
+    minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+  }
+  setInterval(updateTime, 1000);
+  updateTime();
