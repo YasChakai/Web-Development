c43b296b1717e7c123c5ce8fb52cba153f8e54f0
Yassin <y.chakai@icloud.com>
2021-02-12 17:07:03 +0100

    React
    First Component

--- React.js/First-React-Component/src/components/counter.jsx
+++ React.js/First-React-Component/src/components/counter.jsx
@@ -3,24 +3,35 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ['tag1','tag2','tag3']
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({count: this.state.count + 1})
+  }
+
   render() {
-    
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   formatCount() {
     const { count } = this.state;
